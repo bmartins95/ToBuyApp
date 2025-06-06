1b95190ccf1ac56b8e0e6b3d1536f01079ac3f95
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-06-06 15:00:29 -0300

    Add logo to app

---
+++ src/app/home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Image } from 'react-native';
+
+import { styles } from './styles'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@/assets/logo.png')} style={styles.logo}></Image>
+    </View>
+  );
+}
+
+
