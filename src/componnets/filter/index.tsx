f678620c1d33891c0582ed2aaed7124c33643424
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-06-08 15:11:54 -0300

    Add icons to filter

--- src/componnets/filter/index.tsx
+++ src/componnets/filter/index.tsx
@@ -3,6 +3,7 @@ import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
 import { styles } from "./styles"
 
 import { FilterStatus } from "@/types/filter-status";
+import { StatusIcon } from "../status-icon"; 
 
 type Props = TouchableOpacityProps & {
     status: FilterStatus,
@@ -12,6 +13,7 @@ type Props = TouchableOpacityProps & {
 export function Filter({ status, isActive, ...rest }: Props) {
     return (
         <TouchableOpacity style={[styles.container, { opacity: isActive ? 1 : 0.5 }]} activeOpacity={0.8} {...rest} >
+            <StatusIcon status={status} ></StatusIcon>
             <Text style={styles.title}>
                 {status === FilterStatus.DONE ? "Boughts" : "Pending"}
             </Text>
